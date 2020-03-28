aa5cc9467a9c0a5227e4359465ee28a33cca56df
Junior Nascimento <junior.pnascimento@live.com>
2020-03-28 20:55:29 -0300

    atualizado

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -1,30 +1,30 @@
 const connection = require('../database/connection');
 
 module.exports = {
-    async index(request, response) {
-        const { page = 1 } = request.query;
+    async index(request,response){
+        const{page=1} = request.query;
 
         const [count] = await connection('incidents').count();
 
         const incidents = await connection('incidents')
-            .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
-            .limit(5)
-            .offset((page - 1) * 5)
-            .select([
-                'incidents.*', 
-                'ongs.name', 
-                'ongs.email', 
-                'ongs.whatsapp', 
-                'ongs.city', 
-                'ongs.uf'
-            ]);
+        .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
+        .limit(5)
+        .offset((page-1)*5)
+        .select([
+            'incidents.*',
+            'ongs.name',
+            'ongs.email',
+            'ongs.whatsapp',
+            'ongs.city',
+            'ongs.uf'
+        ]);
 
-        response.header('X-Total-Count', count['count(*)']);
+        response.header('X-Total-Count',count['count(*)']);
 
         return response.json(incidents);
     },
 
-    async create(request, response) {
+    async create(request, response){
         const { title, description, value } = request.body;
         const ong_id = request.headers.authorization;
 
@@ -34,8 +34,7 @@ module.exports = {
             value,
             ong_id,
         });
-
-        return response.json({ id });
+        return response.json({id});
     },
 
     async delete(request, response) {
@@ -43,16 +42,15 @@ module.exports = {
         const ong_id = request.headers.authorization;
 
         const incident = await connection('incidents')
-            .where('id', id)
-            .select('ong_id')
-            .first();
+        .where('id', id)
+        .select('ong_id')
+        .first();
 
-        if (incident.ong_id !== ong_id) {
-            return response.status(401).json({ error: 'Operation not permited.'});
+        if(incident.ong_id != ong_id){
+            return response.status(401).json({ error:'Operation not permitted.'});
         }
 
         await connection('incidents').where('id', id).delete();
-
         return response.status(204).send();
     }
 };
--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,17 +1,17 @@
-const crypto = require('crypto');
+const generateUniqueId = require('../utils/generateUniqueId');
 const connection = require('../database/connection');
 
-module.exports = {
-    async index (request, response) {
+module.exports={
+
+    async index(request, response){
         const ongs = await connection('ongs').select('*');
-    
         return response.json(ongs);
     },
 
-    async create(request, response) {
+    async create(request, response){
         const { name, email, whatsapp, city, uf } = request.body;
 
-        const id = crypto.randomBytes(4).toString('HEX');
+        const id = generateUniqueId();
 
         await connection('ongs').insert({
             id,
@@ -21,7 +21,20 @@ module.exports = {
             city,
             uf,
         })
-
-        return response.json({ id });
+        return response.json({id});
     }
 };
+
+/**
+ * Tipos de parâmetros:
+ * 
+ * Query Params: Parâmetros nomeados enviados na rota após "?"(Filtros, paginação)
+ * http://localhost:3333/users?name=Diego
+ * http://localhost:3333/users?page=2&nome=Diego
+ * 
+ * Route Params: Parâmetros utilizados para identificar recursos
+ * http://localhost:3333/users/1
+ * 
+ * request Body: Corpo da requisição, utilizado para criar ou alterar recursos
+ * 
+*/
